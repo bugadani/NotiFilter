3c6bafbdd2f33f0354c1c9deee9cf351e24acee9
Dániel Buga <bugadani@gmail.com>
2021-02-27 09:22:16 +0100

    Add screen on/off listener

--- app/src/main/java/hu/bugadani/notifilter/MainActivity.kt
+++ app/src/main/java/hu/bugadani/notifilter/MainActivity.kt
@@ -1,7 +1,20 @@
 package hu.bugadani.notifilter
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class UnlockReceiver : BroadcastReceiver() {
+    override fun onReceive(appContext: Context?, intent: Intent) {
+        if (intent.action.equals(Intent.ACTION_SCREEN_OFF, ignoreCase = true)) {
+            // SCREEN_OFF: enable proxying notification
+        } else if (intent.action.equals(Intent.ACTION_SCREEN_ON, ignoreCase = true)) {
+            // SCREEN_ON: clear all notifications created by the app
+        }
+    }
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
