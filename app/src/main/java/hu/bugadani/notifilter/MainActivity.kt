bbedff721e87032bc19e2a04f081311fa26b6087
Dániel Buga <bugadani@gmail.com>
2021-02-27 11:11:57 +0100

    Register notification for foreground service

--- app/src/main/java/hu/bugadani/notifilter/MainActivity.kt
+++ app/src/main/java/hu/bugadani/notifilter/MainActivity.kt
@@ -1,6 +1,6 @@
 package hu.bugadani.notifilter
 
-import android.app.Service
+import android.app.*
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -31,6 +31,16 @@ class UnlockReceiver : BroadcastReceiver() {
 class NotificationListener : NotificationListenerService() {
     private var proxied: HashSet<NotificationGroup> = HashSet()
     private val TAG = "NotificationListener"
+    private val channel = NotificationChannel("P", "Proxied Notifications", NotificationManager.IMPORTANCE_LOW).apply {
+        this.description = "The proxied notifications"
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val notificationManager = getSystemService(Service.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+
+        return super.onStartCommand(intent, flags, startId)
+    }
 
     override fun onNotificationPosted(sbn: StatusBarNotification) {
         Log.d(TAG, "Notification posted")
@@ -68,9 +78,31 @@ class NotificationListener : NotificationListenerService() {
 class StartupService : Service() {
     private val receiver = UnlockReceiver()
     private val TAG = "Startup service"
+    private val ONGOING_NOTIFICATION_ID = 1
+    private val channel = NotificationChannel("N", "Foreground Service Notification", NotificationManager.IMPORTANCE_LOW).apply {
+        this.description = "Sorry"
+    }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.d(TAG, "onStartCommand")
+
+        val pendingIntent: PendingIntent =
+                Intent(this, MainActivity::class.java).let { notificationIntent ->
+                    PendingIntent.getActivity(this, 0, notificationIntent, 0)
+                }
+
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+
+        val notification: Notification = Notification.Builder(this, channel.id)
+                .setContentTitle(getText(R.string.notification_title))
+                .setContentText(getText(R.string.notification_message))
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentIntent(pendingIntent)
+                .setTicker(getText(R.string.ticker_text))
+                .build()
+
+        startForeground(ONGOING_NOTIFICATION_ID, notification)
 
         registerReceiver(receiver, IntentFilter(ACTION_SCREEN_ON))
         registerReceiver(receiver, IntentFilter(ACTION_SCREEN_OFF))
