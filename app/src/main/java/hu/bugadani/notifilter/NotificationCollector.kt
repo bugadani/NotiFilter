f10d61f0f3d63481baef73c8b3494d33e62f0841
Dániel Buga <bugadani@gmail.com>
2021-03-11 10:20:36 +0100

    Add database layer

--- app/src/main/java/hu/bugadani/notifilter/NotificationCollector.kt
+++ app/src/main/java/hu/bugadani/notifilter/NotificationCollector.kt
@@ -3,12 +3,18 @@ package hu.bugadani.notifilter
 import android.content.Context
 import android.service.notification.StatusBarNotification
 import android.util.Log
+import androidx.room.Room
+import com.google.gson.Gson
+import hu.bugadani.notifilter.database.NotificationDatabase
+import hu.bugadani.notifilter.database.NotificationRecord
 
-class NotificationCollector(val context: Context) {
+class NotificationCollector(context: Context) {
     private val list = ArrayList<StatusBarNotification>()
+    private val db: NotificationDatabase = NotificationDatabase.getDataBase(context)
 
     init {
-        // TODO Remove old notifications
+        // Remove old notifications
+        db.notificationDao().gc()
     }
 
     fun record(statusBarNotification: StatusBarNotification) {
@@ -17,9 +23,21 @@ class NotificationCollector(val context: Context) {
 
     fun store() {
         if (list.isNotEmpty()) {
+            val gson = Gson()
+            val dao = db.notificationDao()
             Log.d("NotificationCollector", "Storing notifications")
 
-            // TODO save notifications
+            for (sbn in list) {
+                val record = NotificationRecord(
+                    packageId = sbn.packageName,
+                    appName = "",
+                    groupKey = sbn.groupKey,
+                    details = gson.toJson(sbn.notification),
+                )
+
+                val id = dao.insert(record)
+                Log.d("NotificationCollector", "Saved notification with id $id")
+            }
 
             list.clear()
         } else {
